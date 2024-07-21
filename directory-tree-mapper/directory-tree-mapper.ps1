6ba051d61a1b1811f1b9ce24b4c5f9db8281ca40
Aldenor Neto <aldenor.code@gmail.com>
2024-07-21 02:35:37 -0300

    feat: add ignore folders

--- directory-tree-mapper/directory-tree-mapper.ps1
+++ directory-tree-mapper/directory-tree-mapper.ps1
@@ -1,12 +1,13 @@
 param (
-    [int]$MaxDepth = 1
+    [int]$MaxDepth = 4
 )
 
 function Get-DirectoryTree {
     param (
         [string]$Path,
         [int]$Depth,
-        [int]$CurrentDepth = 0
+        [int]$CurrentDepth = 0,
+        [array]$IgnoreFolders
     )
 
     if ($CurrentDepth -ge $Depth) {
@@ -16,10 +17,14 @@ function Get-DirectoryTree {
     $items = Get-ChildItem -Path $Path
 
     foreach ($item in $items) {
+        if ($item.PSIsContainer -and $IgnoreFolders -contains $item.Name) {
+            continue
+        }
+
         $indent = " " * ($CurrentDepth * 2)
         if ($item.PSIsContainer) {
             Write-Output "$indent|-- $($item.Name)/"
-            Get-DirectoryTree -Path $item.FullName -Depth $Depth -CurrentDepth ($CurrentDepth + 1)
+            Get-DirectoryTree -Path $item.FullName -Depth $Depth -CurrentDepth ($CurrentDepth + 1) -IgnoreFolders $IgnoreFolders
         } else {
             Write-Output "$indent|-- $($item.Name)"
         }
@@ -29,5 +34,15 @@ function Get-DirectoryTree {
 # Get the current directory
 $CurrentDirectory = Get-Location
 
+# Ignore folders similar to .gitignore
+$IgnoreFolders = @(
+    'logs', 'pids', 'lib-cov', 'coverage', '.nyc_output', '.grunt',
+    'bower_components', 'build', 'node_modules', 'jspm_packages', 'web_modules',
+    '.npm', '.rpt2_cache', '.rts2_cache_cjs', '.rts2_cache_es', '.rts2_cache_umd',
+    '.yarn-integrity', '.cache', '.parcel-cache', '.next', 'out', '.nuxt', 'dist',
+    '.vuepress', '.temp', '.docusaurus', '.serverless', '.fusebox', '.dynamodb',
+    '.tern-port', '.vscode-test', '.yarn', '.yarn/cache', '.yarn/unplugged'
+)
+
 # Start mapping from the current directory
-Get-DirectoryTree -Path $CurrentDirectory.Path -Depth $MaxDepth
+Get-DirectoryTree -Path $CurrentDirectory.Path -Depth $MaxDepth -IgnoreFolders $IgnoreFolders
