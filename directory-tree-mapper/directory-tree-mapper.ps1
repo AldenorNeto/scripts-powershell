dba5caa5236c17092144f845633fa60d08adf13f
Aldenor Arruda Gomes Neto <74365018+AldenorNeto@users.noreply.github.com>
2025-03-14 13:36:57 -0300

    Update directory-tree-mapper.ps1

--- directory-tree-mapper/directory-tree-mapper.ps1
+++ directory-tree-mapper/directory-tree-mapper.ps1
@@ -36,12 +36,30 @@ $CurrentDirectory = Get-Location
 
 # Ignore folders similar to .gitignore
 $IgnoreFolders = @(
+    # Pastas comuns de Node.js
     'logs', 'pids', 'lib-cov', 'coverage', '.nyc_output', '.grunt',
-    'bower_components', 'build', 'node_modules', '.venv', 'jspm_packages', 'web_modules',
-    '.npm', '.rpt2_cache', '.rts2_cache_cjs', '.rts2_cache_es', '.rts2_cache_umd',
-    '.yarn-integrity', '.cache', '.parcel-cache', '.next', 'out', '.nuxt', 'dist',
-    '.vuepress', '.temp', '.docusaurus', '.serverless', '.fusebox', '.dynamodb',
-    '.tern-port', '.vscode-test', '.yarn', '.yarn/cache', '.yarn/unplugged'
+    'bower_components', 'build', 'node_modules', '.npm', '.yarn-integrity',
+    '.yarn', '.yarn/cache', '.yarn/unplugged', 'jspm_packages', 'web_modules',
+    '.rpt2_cache', '.rts2_cache_cjs', '.rts2_cache_es', '.rts2_cache_umd',
+    '.cache', '.parcel-cache', '.next', 'out', '.nuxt', 'dist', '.vuepress',
+    '.temp', '.docusaurus', '.serverless', '.fusebox', '.dynamodb', '.tern-port',
+    '.vscode-test',
+
+    # Pastas comuns de Python
+    '.venv', 'venv', 'env', '.env', 'ENV', 'env.bak', 'venv.bak', '.pyenv',
+    '__pycache__', '.mypy_cache', '.pytest_cache', '.hypothesis', '.ipynb_checkpoints',
+    '.python-version', '.coverage', '.tox', '.eggs', 'dist', 'build', '*.egg-info',
+    '.pytype', '.ruff_cache',
+
+    # Pastas comuns de PHP
+    'vendor', '.phpunit.result.cache', '.phpunit.cache', 'composer.lock',
+    'storage/framework/cache', 'storage/framework/sessions', 'storage/framework/views',
+    'bootstrap/cache', '.php-cs-fixer.cache',
+
+    # Pastas comuns de C#
+    'bin', 'obj', '.vs', '.vscode', 'packages', 'TestResults', 'artifacts',
+    'Publish', '.dotnet', '.sonarqube', '.local', '.msbuild', '.nuget',
+    '.idea', '.resharper', '*.user', '*.suo', '*.cache', '*.nupkg'
 )
 
 # Start mapping from the current directory
